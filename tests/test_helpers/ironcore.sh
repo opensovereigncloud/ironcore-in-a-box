58d655c08cee3e090df852c182542c469ac8f91c
Felix Riegger <felix.riegger@sap.com>
2026-04-29 15:08:03 +0200

    Build local image and use during test target (#78)
    This ensures image changes will be considered for the tests.
    ironcore-in-a-box users still use the published image.
    
    Also add logs of failed pods

--- tests/test_helpers/ironcore.sh
+++ tests/test_helpers/ironcore.sh
@@ -18,6 +18,23 @@ check_example_machine_ssh() {
     return $?
 }
 
+# dump_failed_pod_logs
+#
+# Find all pods not in "Running" state and dump their logs. This helps diagnose
+# why pods failed to start during cluster setup.
+dump_failed_pod_logs() {
+    local pods
+    pods=$($KUBECTL_CTX get pods -A --no-headers | awk '$4 != "Running" {print $1, $2}')
+    if [ -z "$pods" ]; then
+        log info "No non-running pods found."
+        return 0
+    fi
+    while read -r ns pod; do
+        log err "--- Logs for $ns/$pod ---"
+        log err "$($KUBECTL_CTX logs -n "$ns" "$pod" --all-containers=true --tail=50 2>&1)" || true
+    done <<< "$pods"
+}
+
 # deploy_ironcore
 #
 # Deploy the ironcore-in-a-box kind cluster by running "make up". If a cluster
@@ -31,9 +48,15 @@ deploy_ironcore() {
     if [ "$output" == "$KIND_CLUSTER_NAME" ]; then
         log warn "Re-using existing $KIND_CLUSTER_NAME cluster"
     else
+        log info "Building local kind node image..."
+        docker build -t ironcore-dev/kind-node:local .
         log info "Running 'make up' to spin up ironcore-in-a-box cluster. This may take a while..."
-        make up
+        make up KIND_IMAGE=ironcore-dev/kind-node:local
     fi
-    wait_for 600 pods_ready
+    if ! wait_for 600 pods_ready; then
+        log err "Pods did not become ready in time. Dumping logs of non-running pods:"
+        dump_failed_pod_logs
+        return 1
+    fi
     succeed_for 10 60 pods_ready
 }
